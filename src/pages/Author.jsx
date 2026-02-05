2710de6cd967a1501823e220762b8c8ea1f3b0f6
neosyk-net <runerdycastillo@gmail.com>
2026-02-04 20:44:14 -0500

    fix: remove unused Link import

--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useParams, Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import AuthorBanner from "../images/author_banner.jpg";
 import AuthorItems from "../components/author/AuthorItems";
 import AuthorImageFallback from "../images/author_thumbnail.jpg";
